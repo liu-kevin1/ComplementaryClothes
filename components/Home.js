df8d5700d9ba0486459517e42d8315c7f7ac241e
Mustafo Mustafokulov <maximusphoenix24@gmail.com>
2020-05-28 21:54:11 -0400

    message

--- components/Home.js
+++ components/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 import * as Permissions from 'expo-permissions';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const Clarifai = require('clarifai');
-const app = new Clarifai.App({
-apiKey: 'YOUR_API_KEY'
-});
+// const Clarifai = require('clarifai');
+// const app = new Clarifai.App({
+// apiKey: 'YOUR_API_KEY'
+// });
 
 class Home extends React.Component {    
     constructor(props){
@@ -16,12 +17,28 @@ class Home extends React.Component {
     render() {
         return (
             <View style={styles.container}>
+                <SafeAreaView style = {styles.head}>
+                    <Text style = {{color="#000"}}>Home</Text>
+                </SafeAreaView>
+
             </View>
         );
     }
 }
 
 const styles = Stylesheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+
+    },
+    head: {
+        width: '100%',
+        height: '10%',
+        backgroundColor: "blue",
+        justifyContent:" Center",
+        alignItems: "center"
+    }
 
 });
 
