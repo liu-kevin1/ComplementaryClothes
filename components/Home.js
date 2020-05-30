5f6ec0c0d6e552bba06c9cd96e2da9592ed3b2e4
Mustafo Mustafokulov <maximusphoenix24@gmail.com>
2020-05-29 20:22:42 -0400

    Updated Home.js

--- components/Home.js
+++ components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import { View, Text, Stylesheet, TouchableOpacity, Alert} from 'react-native';
+import { View, Text, Stylesheet, TouchableOpacity, Alert, TouchableHighlight} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
+export default class App extends React.Component{ 
+    render() {
+        return (
+            <AppContainer/>
+        );
+    }
+}
 
 class Home extends React.Component {    
     constructor(props){
@@ -14,28 +22,34 @@ class Home extends React.Component {
                 <SafeAreaView style = {styles.head}>
                     <Text style = {{color:"#000"}}>Home</Text>
                 </SafeAreaView>
-                <View style ={styles.navigation}>
-                    <View style = {styles.nav1}>
-                        {/* <Icon>
-
-                        </Icon> */}
-                        <Text style = {{color: "#000"}}>Home</Text>
+                {/* <View style ={styles.navigation}>
+                    <View style = {styles.nav1}>  /* I think I don't need this but I'm not sure/*
+                        <TouchableHighlight style = {} onPress={}>
+                            <Icon>
+                            </Icon>
+                            <Text style = {{color: "#000"}}>Home</Text>
+                        </TouchableHighlight>
                     </View>
                     <View style = {styles.nav2}>
-                        {/* <Icon>
-                            
-                        </Icon> */}
-                        <Text style = {{color: "#000"}}>Camera</Text>
+                        <TouchableHighlight>
+                            <Icon>
+                                
+                            </Icon>
+                            <Text style = {{color: "#000"}}>Camera</Text>
+                        </TouchableHighlight>
+                        
                     </View>
                     <View style = {styles.nav3}>
-                        {/* <Icon>
-                            
-                        </Icon> */}
-                        <Text style = {{color: "#000"}}>Library</Text>
+                        <TouchableHighlight>
 
+                            <Icon>
+                                
+                            </Icon>
+                            <Text style = {{color: "#000"}}>Library</Text>
+                        </TouchableHighlight>
                     </View>
 
-                </View>
+                </View> */}
 
             </View>
         );
@@ -56,28 +70,52 @@ const styles = StyleSheet.create({
         justifyContent:" Center",
         alignItems: "center"
     },
-    navigation: {
-        flex: 1,
-        width: '100%',
-        height: "15%",
-        backgroundColor: "#DBF3FA",
-        alignItems: "flex-end",
-        justifyContent: "space-evenly",
-        flexDirection: "row",
-    },
-    nav1: {
-        flex: 1,
-        alignItems: "center",
-    },
-    nav2: {
-        flex: 1,
-        alignItems: "center",
-    },
-    nav3: {
-        flex: 1,
-        alignItems: "center",
-    },
-
+    // navigation: {
+    //     flex: 1,
+    //     width: '100%',
+    //     height: "15%",
+    //     backgroundColor: "#DBF3FA",
+    //     alignItems: "flex-end",
+    //     justifyContent: "space-evenly",
+    //     flexDirection: "row",
+    // },
+    // nav1: {
+    //     flex: 1,
+    //     alignItems: "center",
+    // },
+    // nav2: {
+    //     flex: 1,
+    //     alignItems: "center",
+    // },
+    // nav3: {
+    //     flex: 1,
+    //     alignItems: "center",
+    // },
 });
+
+const bottomNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeScreen,
+            navigationOptions: {
+                tabBarIcon: () => (
+                    <Icon name = "home" size = {30} color = {'#bde0eb'} />
+                )
+            }
+        }
+        CameraView: {
+            screen: CameraView,
+            navigationOptions: {
+                tabBarIcon: () => (
+                    <Icon name = "home" size = {30} color = {'#bde0eb'} />
+                )
+            }
+        },
+    },
+    {
+        initialRouteName: 'Home'
+    }
+)
+const AppContainer = createAppContainer(bottomNavigator);
 
 export default Home;
