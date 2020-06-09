1893dcb3273d8f117eba98ee27b81126e74bc28a
3michelin <remingtonskim@gmail.com>
2020-06-09 10:19:06 -0400

    account page complete

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -12,25 +12,7 @@ class Confirmation extends React.Component {
   }
 
   async getRecommendations() {
-    /*call the recommendation ebay api here with the keyword in this.state.classification. this is th onpress of the confirm*/
-    // Tutorial code start
-    var url = "http://svcs.ebay.com/services/search/FindingService/v1";
-    url += "?OPERATION-NAME=findItemsByKeywords";
-    url += "&SERVICE-VERSION=1.0.0";
-    url += "&SECURITY-APPNAME=KevinLiu-CompClo-SBX-a2eb84eea-0ca6a20e";
-    url += "&GLOBAL-ID=EBAY-US";
-    url += "&RESPONSE-DATA-FORMAT=JSON";
-    url += "&callback=_cb_findItemsByKeywords";
-    url += "&REST-PAYLOAD";
-    url += "&" + 'pants';
-    url += "&paginationInput.entriesPerPage=3";
-    fetch(url).then(response => console.log(response)).catch(e => console.log(e));
 
-
-    this.props.navigation.navigate('Recommended', {
-      recommended: /*data we get back from recommendation api */ '',
-      user: this.state.user
-    });
   }
   render() {
     return (
