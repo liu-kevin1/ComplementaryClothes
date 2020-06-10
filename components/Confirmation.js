6ad9c5c6394be775f3ec76a439f667ad8033235c
orgwirelist123 <orgwirelist123@gmail.com>
2020-06-10 09:01:27 -0400

    updated urls and url_pairing, and added comments for future reference

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -16,20 +16,63 @@ class Confirmation extends React.Component{
     async getRecommendations() {
         /*call the api to get products here with the keyword in this.state.classification. this is the onpress of the confirm*/
 
+        // Main Clothing Articles
+        // Pants
+        // Shirts
+        // Shoes
+        // Hats
+        // Socks
+
+        // Accessories
+        // Necklace
+        // Ring
+        // Watch
+        // Bracelet
+
         // var urls = {
         //     pants = "https://www.parsehub.com/api/v2/projects/t3LXvgFShK6y/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
         //     shirts = "https://www.parsehub.com/api/v2/projects/tw8p9tJXSFT5/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
         //     shoes = ,
         //     hats = ,
-        //     socks = 
+        //     socks = ,
+        //     necklace = ,
+        //     ring = ,
+        //     watch = ,
+        //     bracelet = 
         // }
 
+        // Main Clothing Articles
+        // Shoes -> Socks
+        // Dress -> Hats
+        // Top -> Pants
+        // Pants -> Shirts
+        // Hat -> Shirts
+
+        // Accessories
+        // Ring -> Bracelet
+        // Necklace -> Shirts
+        // Sunglasses -> Hats
+        // Bag -> Watch
+        // Necktie -> Shirts
+        // Earrings -> Ring
+        // Watch -> Ring
+        // Bracelet -> Ring
+
         // var url_pairing = {
-        //     shirts = urls[pants],
-        //     pants = urls[shoes],
-        //     shoes = urls[socks],
-        //     socks = urls[hats],
-        //     hats = urls[shirts]
+        //     top = urls["pants"],
+        //     pants = urls["shoes"],
+        //     shoes = urls["socks"],
+        //     socks = urls["hats"],
+        //     hats = urls["shirts"],
+
+        //     ring = urls["bracelet"],
+        //     necklace = urls["shirts"],
+        //     sunglasses = urls["hats"],
+        //     bag = urls["watch"],
+        //     necktie = urls["shirts"],
+        //     earrings = urls["ring"],
+        //     watch = urls["ring"],
+        //     bracelet = urls["ring"]
         // }
 
         //https://www.parsehub.com/api/v2/runs/{RUN_TOKEN}/data
