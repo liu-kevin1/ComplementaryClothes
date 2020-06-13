9a58cbee2f4d0ea4fdcdcdc18f9b991644d72c9f
orgwirelist123 <orgwirelist123@gmail.com>
2020-06-12 20:44:07 -0400

    added the rest of the parsehub links and adjusted the url_pairing table

--- components/Confirmation.js
+++ components/Confirmation.js
@@ -14,69 +14,37 @@ class Confirmation extends React.Component{
     }
 
     async getRecommendations() {
-        /*call the api to get products here with the keyword in this.state.classification. this is the onpress of the confirm*/
+        var urls = {
+             pants: "https://www.parsehub.com/api/v2/projects/t3LXvgFShK6y/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
+             shirts: "https://www.parsehub.com/api/v2/projects/tw8p9tJXSFT5/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
+             shoes: "https://www.parsehub.com/api/v2/projects/tAkYL63zFrYr/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
+             hats: "https://www.parsehub.com/api/v2/projects/ty4wWgwzAdTM/last_ready_run/data?api_key=tBbTET_aiw6F&format=json", 	
+             socks: "https://www.parsehub.com/api/v2/projects/tswSoJj-ZgBt/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
 
-        // Main Clothing Articles
-        // Pants
-        // Shirts
-        // Shoes
-        // Hats
-        // Socks
+             necklace: "https://www.parsehub.com/api/v2/projects/t6BCzgpDmeiu/last_ready_run/data?api_key=tTMbQ11Q_Boi&format=json",
+             ring: "https://www.parsehub.com/api/v2/projects/t5WhmCQr3VR_/last_ready_run/data?api_key=tTMbQ11Q_Boi&format=json",
+             watch: "https://www.parsehub.com/api/v2/projects/tTrL7tTBwYjP/last_ready_run/data?api_key=tTMbQ11Q_Boi&format=json",
+             bracelet: "https://www.parsehub.com/api/v2/projects/t4H5pWYomZbV/last_ready_run/data?api_key=tTMbQ11Q_Boi&format=json"
+        }
 
-        // Accessories
-        // Necklace
-        // Ring
-        // Watch
-        // Bracelet
+        var url_pairing = {
+            top: urls["pants"],
+            pants: urls["shoes"],
+            shoes: urls["socks"],
+            socks: urls["hats"],
+            hats: urls["shirts"],
 
-        // var urls = {
-        //     pants = "https://www.parsehub.com/api/v2/projects/t3LXvgFShK6y/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
-        //     shirts = "https://www.parsehub.com/api/v2/projects/tw8p9tJXSFT5/last_ready_run/data?api_key=tBbTET_aiw6F&format=json",
-        //     shoes = ,
-        //     hats = ,
-        //     socks = ,
-        //     necklace = ,
-        //     ring = ,
-        //     watch = ,
-        //     bracelet = 
-        // }
+            ring: urls["bracelet"],
+            necklace: urls["shirts"],
+            sunglasses: urls["hats"],
+            bag: urls["watch"],
+            necktie: urls["shirts"],
+            earrings: urls["ring"],
+            watch: urls["ring"],
+            bracelet: urls["ring"]
+        }
 
-        // Main Clothing Articles
-        // Shoes -> Socks
-        // Dress -> Hats
-        // Top -> Pants
-        // Pants -> Shirts
-        // Hat -> Shirts
-
-        // Accessories
-        // Ring -> Bracelet
-        // Necklace -> Shirts
-        // Sunglasses -> Hats
-        // Bag -> Watch
-        // Necktie -> Shirts
-        // Earrings -> Ring
-        // Watch -> Ring
-        // Bracelet -> Ring
-
-        // var url_pairing = {
-        //     top = urls["pants"],
-        //     pants = urls["shoes"],
-        //     shoes = urls["socks"],
-        //     socks = urls["hats"],
-        //     hats = urls["shirts"],
-
-        //     ring = urls["bracelet"],
-        //     necklace = urls["shirts"],
-        //     sunglasses = urls["hats"],
-        //     bag = urls["watch"],
-        //     necktie = urls["shirts"],
-        //     earrings = urls["ring"],
-        //     watch = urls["ring"],
-        //     bracelet = urls["ring"]
-        // }
-
-        //https://www.parsehub.com/api/v2/runs/{RUN_TOKEN}/data
-        var url = "https://www.parsehub.com/api/v2/projects/t3LXvgFShK6y/last_ready_run/data?api_key=tBbTET_aiw6F&format=json"
+        var url = urls["pants"];
 
         var text;
         var request = await (fetch(url)
@@ -88,22 +56,31 @@ class Confirmation extends React.Component{
 
         var props_navigation = this.props.navigation
         if (info != undefined) {
-            // var entries = info.split("},");
-            // console.log("for loop");
-            // for (var i = 0; i < 5; i++) {
-            //     var data = entries[i].split(",");
-            //     for (var j = 0; j < data.length; j++) {
-            //         var values = data[j].split(': ');
-            //         var key = values[0];
-            //         var value = values[1];
-            //         console.log(key);
-            //     }
-            // } 
-            //console.log(text);
-            console.log(info.items[1]); 
+            var values = ["name", "image"]
+            var return_count = 2;
+            var recommended_items = [];
 
+            var info_items_length = Object.keys(info.items).length
+            
+            // Sort through info.items and pick out only items that have all the necessary data
+            for (var i = 2; i < info_items_length; i++) {
+                var validItem = true
+                for (var j = 0; j < values.length; j++) {
+                    if (info.items[i] == undefined || info.items[i][values[j]] == undefined) {
+                        validItem = false;
+                        j = values.length;
+                    }
+                }
+                if (validItem) {
+                    recommended_items.push(info.items[i]);
+                    if (recommended_items.length == return_count) {
+                        i = info_items_length;
+                    }
+                }
+            }
+            console.log(recommended_items[0]);
             props_navigation.navigate('Recommended', {
-                recommendations: [info.items[0], info.items[1]] /*data we get back from the api */
+                recommendations: [recommended_items[0], recommended_items[1]] /*data we get back from the api */
             })
         }
         
